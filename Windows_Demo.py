fbae817ed042e05997baa5cd888eeae7ecdfcfd0
Peter Vandenabeele <peter@vandenabeele.com>
2014-12-05 18:14:10 +0100

    Send CPU temperature to the server, keep receiving a boolean

--- Windows_Demo.py
+++ Windows_Demo.py
@@ -3,18 +3,20 @@
 #important: before running this demo, make certain that you import the library
 #'paho.mqtt.client' into python (https://pypi.python.org/pypi/paho-mqtt)
 
-import allthingstalk_arduino_standard_lib as IOT                              #provide cloud support
+import allthingstalk_arduino_standard_lib as IOT       #provide cloud support
 from time import sleep                             #pause the app
+from subprocess import call
+import os
 
 #set up the ATT internet of things platform
-IOT.on_message = on_message
-IOT.ClientId = "put your client id here"
-IOT.ClientKey = "put your client key here"
-IOT.DeviceId = "put your device id here"
+# IOT.on_message = on_message
+IOT.ClientId = <client_id>
+IOT.ClientKey = <client_key>
+IOT.DeviceId = <device_id>
 
-In1Name = "Put the name of your sensor"                                #name of the button
-In1Id = 1                                                            #the id of the button, don't uses spaces. required for the att platform
-Out1Name = "Put the name of your actuator"
+In1Name = "foo"                                #name of the button
+In1Id = 1                                      #the id of the button, don't uses spaces. required for the att platform
+Out1Name = "bar"
 Out1Id = 2
 
 
@@ -36,19 +38,15 @@ def on_message(id, value):
 
 #make certain that the device & it's features are defined in the cloudapp
 IOT.connect()
-IOT.addAsset(In1Id, In1Name, "put your description here", False, "bool")
-IOT.addAsset(Out1Id, Out1Name, "put your description here", True, "bool")
+IOT.addAsset(In1Id, In1Name, "In1Id", False, "String")
+IOT.addAsset(Out1Id, Out1Name, "Out1Id", True, "bool")
 IOT.subscribe()                                        		#starts the bi-directional communication
 
 nextVal = True;
 #main loop: run as long as the device is turned on
 while True:
-    if nextVal == True:
-        print(In1Name + " activated")
-        IOT.send("true", In1Id)
-        nextVal = False
-    else:
-        print(In1Name + " deactivated")
-        IOT.send("false", In1Id)
-        nextVal = True
-    sleep(5)
+    temp = os.popen("/Users/peter_v/Documents/data/projects/temp/osx-cpu-temp/osx-cpu-temp").read()
+    temp = temp[:2] + temp[3:4]
+    print("temp is " + temp)
+    IOT.send(temp, In1Id)
+    sleep(2)
